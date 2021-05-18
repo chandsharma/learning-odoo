e78bf0908836f7d86f228b49e0eb5cb2ddd4911a
Chandan <chandanlalka@gmail.com>
2021-05-18 17:31:37 +0530

    created patient model/view/security-permissions/tree-view/list-view/compute-method-for-age

--- models/models.py
+++ models/models.py
@@ -1,18 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+from datetime import date, datetime
+
+class hospital_patients (models.Model):
+    _name = 'hospital.patients'
+
+    #@api.onchange('dob')
+    def _age(self):
+        if self.dob:
+            dob = datetime.strptime(str(self.dob), "%Y-%m-%d")
+            age_calc = (datetime.today() - dob).days/365
+            print('\n\n\n')
+            print(str(self.dob))
+            print(int(age_calc))
+            self.age = int(age_calc)
+
+    name = fields.Char(
+        string='name',
+    )
+    dob = fields.Date(string='DOB')
+    image = fields.Binary("Image")
+    diseases = fields.Char(string='Diseases')
+    age = fields.Integer(string='Age (years)',compute="_age")
+    # name = fields.Char(default=lambda self: self._default_name())
 
 
-# class hospital_management(models.Model):
-#     _name = 'hospital_management.hospital_management'
-#     _description = 'hospital_management.hospital_management'
+class hospital_staff (models.Model):
+    _name = 'hospital.staff'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    @api.onchange('dob')
+    def _age(self):
+        if self.dob:
+            dob = datetime.strptime(str(self.dob), "%Y-%m-%d")
+            age_calc = (datetime.today() - dob).days/365
+            print('\n\n\n')
+            print(str(self.dob))
+            print(int(age_calc))
+            self.age = int(age_calc)
+    name = fields.Char(
+        string='name',
+    )
+    dob = fields.Date(string='DOB')
+    age = fields.Integer(string='Age (years)',compute="_age")
+
+    
+    
